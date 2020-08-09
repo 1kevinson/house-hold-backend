4222c0a113bbbb87d14c0aba358f26a1992f957e
Arsene Kevin <kevinkouomeu@icloud.com>
2020-08-09 20:13:11 +0200

    well configured the database with mysql

--- api.household/src/main/java/com/project/household/api/Controller/UserController.java
+++ api.household/src/main/java/com/project/household/api/Controller/UserController.java
@@ -1,37 +1,32 @@
 package com.project.household.api.Controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.project.household.api.Entity.User;
 import com.project.household.api.Repositiory.UserRepository;
 
+@RestController
+@RequestMapping("/api")
 public class UserController {
 
-	@Controller
-	@RequestMapping(path = "/demo")
-	public class MainController {
+	private final UserRepository userRepository = null;
 
-		@Autowired
-		private UserRepository userRepository;
-
-		@PostMapping(path = "/add")
-		public @ResponseBody String addNewUser(@RequestParam String name, @RequestParam String email) {
-			User n = new User();
-			n.setName(name);
-			n.setEmail(email);
-			userRepository.save(n);
-			return "Saved";
-		}
-
-		@GetMapping(path = "/all")
-		public @ResponseBody Iterable<User> getAllUsers() {
-			return userRepository.findAll();
-		}
+	@PostMapping(path = "/add")
+	public String addNewUser(@RequestParam String name, @RequestParam String email) {
+		User n = new User();
+		n.setName(name);
+		n.setEmail(email);
+		userRepository.save(n);
+		return "Saved";
 	}
+
+	@GetMapping(path = "/all")
+	public Iterable<User> getAllUsers() {
+		return userRepository.findAll();
+	}
+
 }
